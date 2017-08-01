9b3963ccfe7051d40c7fc63d017fa34173743e2f
Vignesh Karthikeyan <vikki@pal-nat186-118-187.itap.purdue.edu>
2017-08-01 13:35:23 -0400

    first commit

---
+++ UdemyJS/Javascript/game.js
@@ -0,0 +1,10 @@
+var weAreHere = "False";
+while(weAreHere === "False")
+{
+	var response = prompt("Are we there yet?");
+	if(response === "yes"||response === "yeah"||response === "shit dude, there's playboy carti"){
+		weAreHere = "True";
+		alert("weAreHere")
+	}
+
+}
