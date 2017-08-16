646ea5604a0541bfed528a29a003f59f08beef54
Vignesh Karthikeyan <vikki@pal-nat186-126-129.itap.purdue.edu>
2017-08-16 13:24:22 -0400

    starting project

---
+++ UdemyJS/Jquery/On.js
@@ -0,0 +1,4 @@
+// similar to on click listener
+$('button').on('click', function(){
+	console.log("button clicked!");
+});
---
+++ UdemyJS/Jquery/effects.js
@@ -0,0 +1,5 @@
+$('button').on("click", function(){
+	$('img').fadeOut(1000);
+	// $('img').fadeToggle(1000);
+	
+})
