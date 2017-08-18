85374958163f2c76efb667b96e3da1977c4777c1
Vignesh Karthikeyan <vikki@pal-nat186-124-171.itap.purdue.edu>
2017-08-18 15:26:49 -0400

    finished the todo project :)

--- UdemyJS/Jquery/ToDoList/assets/js/todos.js
+++ UdemyJS/Jquery/ToDoList/assets/js/todos.js
@@ -1,6 +1,31 @@
 
 // when selecting an li, it must toggle between grey & strike through and black
-$('li').on("click",function(){
+// can only use jquery on code that is run the first time so this needs to be attached to the ul rather than the li
+$('ul').on("click","li",function(event){
 	$(this).toggleClass("completed");
 });
 
+$('ul').on("click","span",function(event){
+	$(this).parent().remove();
+	event.stopPropagation();
+});
+
+// add new items
+$('input').keypress(function(event){
+	if(event.which === 13){
+		// extracts number
+		var newEntry=$(this).val();
+		// just clears the entry box
+		$(this).val("");
+		// formatting
+		$('ul').append("<li><span><i class='fa fa-trash-o' aria-hidden='true'></i> </span>" + newEntry + "</li>");  
+		// add this in somehow <i class="fa fa-trash-o" aria-hidden="true"></i> 
+
+	}
+});
+
+// fade out input when '+'' is hit
+$('.fa-plus').on("click",function(){
+	$('input').fadeToggle();
+
+});
