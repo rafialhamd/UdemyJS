2f923b147f7100977bb5c4cf174b4459b85a2fb2
meshugah <vikkikarthikeyan@gmail.com>
2017-12-27 22:09:59 +0000

    more Express! :)

--- UdemyJS/ExpressJSIntermediate/app.js
+++ UdemyJS/ExpressJSIntermediate/app.js
@@ -1,26 +1,31 @@
 var express = require("express");
 var app = express();
+//including public directory
+app.use(express.static("public"));
+app.set("view engine", "ejs");
 
 //root route haha
 app.get("/",function(req,res){
-    res.render("home.ejs"); //embedded js
+    res.render("home"); //embedded js
+});
+
+app.get("/fallinlovewith/:thing", function(req, res){
+    var thing = req.params.thing;
+    res.render("love", {thingVar: thing});
 });
 
 //new route
 app.get("/posts",function(req, res){
-   var posts = [
+  var posts = [
             {title: "Post 1", author: "Susy"},
             {title: "My adorable pet bunny", author: "Colt"},
             {title: "Can you believe this is an actual doggo?", author: "Colt"}
-       ];
+      ];
        
-    res.render("posts.ejs", {posts: posts});   
+    res.render("posts", {posts: posts});   
 });
 
-app.get("/fallinlovewith/rusty/:thing", function(req, res){
-    var thing = req.params.thing;
-    res.render("love.ejs",{thingVar: thing});
-});
+
 
 
 
